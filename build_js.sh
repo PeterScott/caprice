4c1ea7bd3ceee4d79585095879e53f8c5de56de4
Peter Scott <pjscott@iastate.edu>
2010-11-07 16:40:08 -0600

    Renamed some files

--- build_js.sh
+++ build_js.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd client_js
-java -jar compiler.jar --js pubsubcore-client.js --js pubsub_connect.js --js aimo.js --js coalesce.js --js mars.js --js syncstring.js --js diff_match_patch.js --js sync-textarea.js --js_output_file ../webroot/js/script.js  --compilation_level WHITESPACE_ONLY
+java -jar compiler.jar --js pubsubcore-client.js --js pubsub_connect.js --js aimo.js --js coalesce.js --js protocol.js --js syncstring.js --js diff_match_patch.js --js sync-textarea.js --js_output_file ../webroot/js/script.js # --compilation_level WHITESPACE_ONLY
